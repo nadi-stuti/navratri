189a4daac3c02b65fcea20d719787f0c9e96cbcf
Nulligma <shantanu@nulligma.com>
2025-09-08 20:22:25 +0530

    Fix app ID and name in capacitor.config.ts; update package name in package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.nulligma.fansting_navratri",
-  appName: "Fasting recipes and navratri guide",
+  appId: "com.nulligma.fasting_navratri",
+  appName: "Fasting & Navratri",
   webDir: "dist",
+  android: {
+    buildOptions: {
+      keystorePath:
+        "C:/Users/Admin/OneDrive/keys/Android_keystore/navratri_key_pass_123456.keystore",
+      keystorePassword: "123456",
+      keystoreAlias: "key0",
+      keystoreAliasPassword: "123456",
+    },
+  },
 };
 
 export default config;
